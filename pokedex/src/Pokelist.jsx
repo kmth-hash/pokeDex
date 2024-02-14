fdd3fe0265ffecad130b0ec979ec658b2a1be1ca
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-02-15 01:30:39 +0530

    get parameters from URL

--- pokedex/src/Pokelist.jsx
+++ pokedex/src/Pokelist.jsx
@@ -1,9 +1,23 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaUserCircle } from "react-icons/fa";
+import { useParams } from "react-router-dom";
+import { useEffect , useState } from "react";
 // import './MainPage.css';
 import "./stylesheets/Pokelist.css";
 
 function Pokelist() {
+  const {pokemon} = useParams();
+
+  useEffect(() => {
+    //get pokemon name from URL using useParams()
+    console.log(pokemon);
+
+    return () => {  
+      
+    }
+  }, [])
+  
+
   return (
     <div className="pokepage">
       <div className="MPContainer1 d-flex ">
@@ -49,9 +63,9 @@ function Pokelist() {
             <h4 className="text-center fw-bold m-0">Victini</h4>
             <h6 className="text-center m-0">Psychic · Fire</h6>
           </div>
-          {Array.from(Array(10).keys()).map((n) => {
+          {Array.from(Array(10).keys()).map((n,ind) => {
             return (
-              <div className="pokeblock col-lg-2 col-md-3 col-xs-3" key={n}>
+              <div className="pokeblock col-lg-2 col-md-3 col-xs-3" key={ind}>
                 <div className="pokeImg text-center">
                   <img
                     src="https://img.pokemondb.net/sprites/black-white/normal/victini.png"
