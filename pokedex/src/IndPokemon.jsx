a6722274b8961514be2bd11690df61c5380b3c65
arohidsilva <arohidsilva311@gmail.com>
2024-02-20 18:56:02 +0530

    Passing parameter fixed in IndPokemon

--- pokedex/src/IndPokemon.jsx
+++ pokedex/src/IndPokemon.jsx
@@ -4,8 +4,18 @@ import React from 'react';
 import { IoMdArrowRoundBack } from "react-icons/io";
 import "./stylesheets/IndPokemon.css";
 import { GrFormPrevious, GrFormNext} from "react-icons/gr";
+import infoData from "./pokeInfo.json";
+import { useLocation } from 'react-router-dom';
 
 function IndPokemon() {
+  const pokemon = useLocation().pathname.split("/").pop().toLowerCase();
+  console.log("pokemon",pokemon);
+
+  const found = infoData.find(ele=>{
+    return ele.pokemon === pokemon;
+  })
+
+  console.log("pokemon data:",found);
   return (
     <div>
       <div className="IPContainer1 d-flex ">
@@ -13,7 +23,7 @@ function IndPokemon() {
           <IoMdArrowRoundBack />  
         </div>
         <div className="IPTitle">
-          <h3>Victini</h3>
+          <h3>{found.pokemon}</h3>
         </div>
       </div>
       <div className='IPNavigate'>
@@ -28,7 +38,7 @@ function IndPokemon() {
       <div className='container'>
       <div className='IPDataContainer row'>
         <div className='IPImage col-lg-6 col-md-6 col-xs-3'>
-          <img src="https://img.pokemondb.net/artwork/victini.jpg" width="322" height="400" alt="Victini artwork by Ken Sugimori" />
+          <img src={found.pokeImg} width="322" height="400" alt="Victini artwork by Ken Sugimori" />
         </div>
         <div className='IPPokeData col-sm'>
           <div className='IPDataTitle1'>Pokédex data</div>
@@ -36,28 +46,32 @@ function IndPokemon() {
             <tbody>
               <tr>
                 <th>National No. </th>
-                <td><strong>0494</strong></td>
+                <td><strong>{found.NationalID}</strong></td>
               </tr>
               <tr>
                 <th>Type</th>
                 <td>
                   <div className='d-flex'>
-                    <div className="IPType">Grass</div>
-                    <div className="IPType">Poison</div>
+                  {Array.from(found.pokeTypes).map((i, index)=>{
+                    return(
+                      <div className="IPType" key={index}>{i}</div>
+                    )
+                    })
+                  }
                   </div>
                 </td>
               </tr>
               <tr>
                 <th>Species</th>
-                <td>Victory Pokémon</td>
+                <td>{found.Species}</td>
               </tr>
               <tr>
                 <th>Height</th>
-                <td>0.4 m (1'04")</td>
+                <td>{found.Height}</td>
               </tr>
               <tr>
                 <th>Weight</th>
-                <td>4.0 kg (8.8 lbs)</td>
+                <td>{found.Weight}</td>
               </tr>
             </tbody>
           </table>
